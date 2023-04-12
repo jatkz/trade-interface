5c276083af1a46db61625d2c72b1d9e30247c0f0
jaredt <jaredt@jt.local>
2023-04-12 11:01:15 -0500

    arbitrary code

--- components/trade/titlecard.tsx
+++ components/trade/titlecard.tsx
@@ -5,46 +5,44 @@ import styles from "./titlecard.module.css";
 
 export default function TitleCard() {
   return (
-    <Pane height="100vh" width="100vw" padding={4}>
-      <Card
+    <Card
+      display="flex"
+      flex="auto"
+      flexDirection="column"
+      padding={8}
+      border={true}
+    >
+      <Pane display="flex" justifyContent="space-between" paddingY={4}>
+        <Pane display="flex" alignItems="center">
+          <Text marginRight={8}>TSLA</Text>
+          <Image
+            priority
+            src="/images/tsla.png"
+            height={50}
+            width={50}
+            alt="Tesla"
+            className={styles.borderCircle}
+          />
+        </Pane>
+        <Pane display="flex" flexDirection="column">
+          <Text>$105 estimated</Text>
+          <Text>$56 Realtime</Text>
+        </Pane>
+      </Pane>
+
+      <Pane
         display="flex"
         flex="auto"
-        flexDirection="column"
-        padding={8}
-        border={true}
+        justifyContent="space-between"
+        paddingY={4}
       >
-        <Pane display="flex" justifyContent="space-between" paddingY={4}>
-          <Pane display="flex" alignItems="center">
-            <Text marginRight={8}>TSLA</Text>
-            <Image
-              priority
-              src="/images/tsla.png"
-              height={50}
-              width={50}
-              alt="Tesla"
-              className={styles.borderCircle}
-            />
-          </Pane>
-          <Pane display="flex" flexDirection="column">
-            <Text>$105 estimated</Text>
-            <Text>$56 Realtime</Text>
-          </Pane>
+        <Pane display="flex">
+          <Text># 3sigmamargins, bullish</Text>
         </Pane>
-
-        <Pane
-          display="flex"
-          flex="auto"
-          justifyContent="space-between"
-          paddingY={4}
-        >
-          <Pane display="flex">
-            <Text># 3sigmamargins, bullish</Text>
-          </Pane>
-          <Pane display="flex">
-            <Text>Score: 565</Text>
-          </Pane>
+        <Pane display="flex">
+          <Text>Score: 565</Text>
         </Pane>
-      </Card>
-    </Pane>
+      </Pane>
+    </Card>
   );
 }
